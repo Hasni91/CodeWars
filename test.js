62bbcaddc2af71d3bb00aaa833f4d18f2766b69b
Hasni Fodeilla <hasni1.fodeilla@epitech.eu>
2022-06-30 09:55:58 +0200

    commit

--- test.js
+++ test.js
@@ -28,7 +28,8 @@ function isPangram(string){
             res = false;
         }
     }
-    console.log("res est",res);
+    // console.log("res est",res);
+    return res;
 }
 // isPangram("The quick brown fox jumps over the lazy dogt");
 
@@ -85,4 +86,145 @@ function add(num1, num2) {
 
     console.log( parseInt(res.join('')) );
 }
+
 // add(1,30);
+
+function squareDigits(nb){
+
+    let res = "";
+
+    if(typeof(nb) == "number"){
+        let str  = nb.toString();
+        let tab = str.split("");
+        for(var i = 0; i < tab.length; i++){
+            res += tab[i] * tab[i];
+        }
+    }
+    return parseInt(res) ;
+}
+// squareDigits(9119);
+
+function filter_list(l) {
+    let res = [];
+    for(let i = 0; i < l.length; i++){
+        if(typeof(l[i]) == "number"){
+            res = [...res,l[i]];
+        }
+    }
+    return res;
+}
+// filter_list([10,20,'30',"coucou",34]);
+
+function explode(arg){
+    let str = "";
+    if(Array.isArray(arg)){
+        for(let i = 0 ; i < arg.length; i++){
+            for(let j = 0; j < arg[i].length; j++){
+                for(let k = 0 ; k < parseInt(arg[i][j]); k++){
+                    str += arg[i][j];
+                }
+            }
+        }
+    }else{
+        for(let i = 0 ; i < arg.length; i++){
+            for(let j = 0 ; j < parseInt(arg[i]); j++){
+                str += arg[i];
+            }
+        }
+    }
+    return str;
+}
+// console.log(explode(["312", "333122"]));
+// console.log(explode("3100002"));
+
+function solution(number){
+    let res = 0;
+    for(let i = 1; i < number; i++){
+        if(i % 3 === 0 || i % 5 === 0 ){
+            res += i;
+        }
+    }
+    return res;
+}
+// solution(23);
+
+function isPangram(str){
+    
+    let tab = [];
+    let alpabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+    let newAlpha = alpabet.replace(/\s/g, '');
+    let newStr = str.toLowerCase();
+    newStr = newStr.replace(/\s/g, '');
+
+    for(let i = 0; i < newStr.length; i++){
+        
+        for(let j = 0; j < newAlpha.length; j++){
+            if(newStr[i] === newAlpha[j]){
+                tab = [...tab,newStr[i]];
+            }
+        }
+    }
+    let res = tab.filter((item, index) => tab.indexOf(item) === index);
+
+    if(res.length === 26)
+        return true;
+    else{
+        return false;
+    }
+}
+// isPangram("The quick brown fox jumps over the lazy dog");
+function isValidIP(str){
+    let regex = /^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$/gm;
+    return regex.test(str);
+}
+// console.log(isValidIP("1.2.3.4\n"));
+
+function accum(str){
+	newStr = "";
+    for(let i = 0; i < str.length; i++){
+        for(let j = 0 ; j <= i; j++){
+            if(j == 0){
+                newStr += str[i].toUpperCase();
+            }else{
+                newStr += str[i].toLowerCase() ;
+            }
+            if(j === i){
+                newStr += "-" ;
+            }
+        }
+    }
+    return newStr.slice(0, -1);
+}
+// console.log(accum("ZpglnRxqenU"));
+
+function unusedDigits(arg) {
+    let str_nb = "0123456789";
+    console.log(str_nb.indexOf("toto"));
+    for(let i = 0; i<arg.length; i++){
+        for(let j = 0; j < arg[i].toString().length; j++){
+            console.log(arg[i][j]);
+        }
+    }
+}
+// unusedDigits([12, 34, 56, 78]);
+
+function anagrams(str, tab) {
+
+    for(let i = 0; i < tab.length; i++){
+        if(str.length == tab[i].length){
+            for(let j = 0; j < tab.length; j++){
+                console.log(tab[i][j]);
+            }
+        }
+    }
+}
+// anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']);
+
+function isPrime(nbr){
+    
+    if(nbr < 0) return false;
+    for(var i = 2; i < nbr; i++)
+    if(nbr%i === 0) return false;
+  return nbr > 1;
+}
+// console.log(isPrime(-3));
